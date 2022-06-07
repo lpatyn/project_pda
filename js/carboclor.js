6de12e267b6349dfe4fbfb426f467488f9ddab7e
Hernán Cañete <94782699+onlihernan@users.noreply.github.com>
2022-06-07 18:24:07 -0300

    Carboclor - tariff update
    Port pilot tariff was updated.

--- js/carboclor.js
+++ js/carboclor.js
@@ -44,7 +44,7 @@ function generarPda(){
     }
     
     let tarifa = uf * 14;
-    portPilot = ((tarifa + 2200) * 2) + (tarifa * arrDraft) + (tarifa * depDraft);
+    portPilot = ((tarifa + 2320) * 2) + (tarifa * arrDraft) + (tarifa * depDraft);
 
     document.getElementById("PDA").innerHTML = `${vesselName.toUpperCase()} – CARBOCLOR – ${portStay} day${plural} along:<br/>
     ----------------------------------------<br/>
