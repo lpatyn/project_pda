aa5daa72a442668002034d3c514539841fdac9da
Hernán Cañete <94782699+onlihernan@users.noreply.github.com>
2022-06-07 18:39:52 -0300

    Vitco - tariff update
    Port pilot tariff was updated.

--- js/vitco.js
+++ js/vitco.js
@@ -48,7 +48,7 @@ function generarPda(){
     }
     
     let tarifa = uf * 14;
-    portPilot = ((tarifa + 2200) * 2) + (tarifa * arrDraft) + (tarifa * depDraft);
+    portPilot = ((tarifa + 2370) * 2) + (tarifa * arrDraft) + (tarifa * depDraft);
 
     document.getElementById("PDA").innerHTML = `${vesselName.toUpperCase()} – VITCO – ${portStay} day${plural} along:<br/>
     -----------------------------------<br/>
