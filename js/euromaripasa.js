dda6b124fdcb726226415a9e30bb71748296e566
Hernán Cañete <94782699+onlihernan@users.noreply.github.com>
2024-03-14 16:46:19 -0300

    Update euromaripasa.js
    LP updated

--- js/euromaripasa.js
+++ js/euromaripasa.js
@@ -20,6 +20,7 @@ function generarPda(){
     let uf = Math.ceil((loa * beam * dm) / 800);
     let plural = "";
     let muelle = document.getElementById("muelle").value;
+    let freePratique = (((nrt-1001)*6.9429+416574)/840)+50;
     document.getElementById("PDA").style = "background-color: white; box-shadow: 10px 10px 10px rgba(0,0,0,0.3)"
 
     if(portStay != 1){
@@ -67,7 +68,7 @@ function generarPda(){
     }
 
     if(!cabotajeIn.checked){
-        libreSigres = `-Free Pratique: usd 490.<br/>
+        libreSigres = `-Free Pratique: usd ${Math.ceil(freePratique).toLocaleString("en-US")}.<br/>
         -Garbage Insp: &nbsp;usd 290.<br/>`;
     }
 
