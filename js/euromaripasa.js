b26b513088ca6e2b11bba96e79a61c50751910ee
Hernán Cañete <94782699+onlihernan@users.noreply.github.com>
2024-09-03 10:19:18 -0300

    Update euromaripasa.js
    Euroamerica & Maripasa light dues update

--- js/euromaripasa.js
+++ js/euromaripasa.js
@@ -10,7 +10,7 @@ function generarPda(){
     let cabotajeIn = document.getElementById("cabotajeIn");
     let cabotajeOut = document.getElementById("cabotajeOut");
     let portDues = 0.26 * nrt * portStay;
-    let lightDues = nrt * 0.05801;
+    let lightDues = nrt * 0.029;
     let ispsCharge = 950;
     let portPilot = 0;
     let headClerk = 1100;
