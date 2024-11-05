f25990ef06ceae4079aab42100433c0118f99110
Hernán Cañete <94782699+onlihernan@users.noreply.github.com>
2024-11-05 13:29:35 -0300

    Update euromaripasa.js
    Light Dues and Migrations update

--- js/euromaripasa.js
+++ js/euromaripasa.js
@@ -10,7 +10,7 @@ function generarPda(){
     let cabotajeIn = document.getElementById("cabotajeIn");
     let cabotajeOut = document.getElementById("cabotajeOut");
     let portDues = 0.26 * nrt * portStay;
-    let lightDues = nrt * 0.029;
+    let lightDues = nrt * 0.058;
     let ispsCharge = 950;
     let portPilot = 0;
     let headClerk = 1100;
@@ -73,13 +73,13 @@ function generarPda(){
     }
 
     if(!cabotajeIn.checked && !cabotajeOut.checked){
-        migra = `-Migrations: &nbsp;&nbsp;&nbsp;usd 2,000. (In/out)<br/>`
+        migra = `-Migrations: &nbsp;&nbsp;&nbsp;usd 2,500. (In/out)<br/>`
     }
     else if(!cabotajeIn.checked && cabotajeOut.checked){
-        migra = `-Migrations: &nbsp;&nbsp;&nbsp;usd 1,000. (In)<br/>`
+        migra = `-Migrations: &nbsp;&nbsp;&nbsp;usd 1,250. (In)<br/>`
     }
     else if(cabotajeIn.checked && !cabotajeOut.checked){
-        migra = `-Migrations: &nbsp;&nbsp;&nbsp;usd 1,000. (Out)<br/>`
+        migra = `-Migrations: &nbsp;&nbsp;&nbsp;usd 1,250. (Out)<br/>`
     }
     
     let tarifa = uf * 14;
