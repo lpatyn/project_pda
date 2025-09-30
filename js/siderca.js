ba9a594e1727668a060c630917585ec59550eecf
Hernán Cañete <94782699+onlihernan@users.noreply.github.com>
2025-09-30 10:51:31 -0300

    Update siderca.js
    Siderca port pilot update

--- js/siderca.js
+++ js/siderca.js
@@ -44,7 +44,7 @@ function generarPda(){
     }
     
     let tarifa = uf * 14;
-    portPilot = ((tarifa + 2320) * 2) + (tarifa * arrDraft) + (tarifa * depDraft);
+    portPilot = ((tarifa + 2720) * 2) + (tarifa * arrDraft) + (tarifa * depDraft);
 
     document.getElementById("PDA").innerHTML = `${vesselName.toUpperCase()} – SIDERCA – ${portStay} day${plural} along:<br/>
     --------------------------------------<br/>
